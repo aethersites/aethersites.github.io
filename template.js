30c5cd97ef23718233bfe83659f97859a99bef30
aethersites <aethersites@gmail.com>
2025-08-27 14:24:11 +0200

    Update template.js

--- template.js
+++ template.js
@@ -4,12 +4,13 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "redacted", // <-- replace with your actual key
-  authDomain: "redacted",
-  projectId: "redacted",
-  storageBucket: "redacted",
-  messagingSenderId: "redacted",
-  appId: "redacted"
+  apiKey: "AIzaSyCOHC_OvQ4onPkhLvHzZEPazmY6PRcxjnw",
+    authDomain: "goodplates-7ae36.firebaseapp.com",
+    projectId: "goodplates-7ae36",
+    storageBucket: "goodplates-7ae36.firebasestorage.app",
+    messagingSenderId: "541149626283",
+    appId: "1:541149626283:web:928888f0b42cda49b7dcee",
+    measurementId: "G-HKMSHM726J"
 };
 
 // Initialize Firebase
