bb6652e0e44de83a81d50146adf4d1d09b636afe
aethersites <aethersites@gmail.com>
2025-08-30 14:03:56 +0200

    Update auth-detect.js

--- auth-detect.js
+++ auth-detect.js
@@ -13,10 +13,13 @@
 
   const firebaseConfig = {
     // <-- REPLACE with your Firebase config
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    // ...
+    apiKey: "AIzaSyCOHC_OvQ4onPkhLvHzZEPazmY6PRcxjnw",
+    authDomain: "goodplates-7ae36.firebaseapp.com",
+    projectId: "goodplates-7ae36",
+    storageBucket: "goodplates-7ae36.firebasestorage.app",
+    messagingSenderId: "541149626283",
+    appId: "1:541149626283:web:928888f0b42cda49b7dcee",
+    measurementId: "G-HKMSHM726J"
   };
 
   const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
